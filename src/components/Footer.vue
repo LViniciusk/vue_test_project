560228708847295b051744b8b17d03dbe876428f
LViniciusk <botlink2030@gmail.com>
2024-02-29 23:02:48 -0300

    #

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <p>Make Yout Burger &copy; 2024</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+    footer{
+        background: #222;
+        color: #FFF;
+        padding: 20px;
+        text-align: center;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
